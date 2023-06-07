bebe3e45ba9625b04a446365d91b54f5bdef228b
Satyam2192 <Satyam21092@gmail.com>
2023-06-07 14:55:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Footer } from './components/Footer'
+import {Header } from './components/Header'
+import {MainPage } from './components/MainPage'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/components/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
